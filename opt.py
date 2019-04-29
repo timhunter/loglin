a7a616f531df92d40bf9d9ecd3417a686b085ec6
Tim Hunter <timhunter@ucla.edu>
2019-04-28 18:50:31 -0700

    Moved lots of functionality into a class LogLinModel; the diff looks ugly, but not much has actually changed

--- opt.py
+++ opt.py
@@ -60,24 +60,6 @@ def read_input(filename):
                     raise
             (line, linenum) = (fp.readline(), linenum+1)
 
-# extract_model returns a pair (m,dim), where
-#   - m is a mapping from LHSs to mappings from RHSs to feature vectors
-#   - dim is the number of parameters
-def extract_model(inp):
-    m = defaultdict(lambda: {})
-    dim = None
-    for (lhs, rhs, feats) in inp:
-        if dim is None:
-            dim = len(feats)
-        else:
-            assert dim == len(feats), "Mismatching dimensions"
-        old_feats = m[lhs].get(rhs, None)
-        if old_feats is not None:
-            assert np.array_equal(old_feats, feats), ("Mismatching features for lhs %s and rhs %s" % (lhs,rhs))
-        else:
-            m[lhs][rhs] = np.array(feats)
-    return (m,dim)
-
 # extract_training_data returns a mapping from (LHS,RHS) pairs to frequencies
 def extract_training_data(inp):
     td = defaultdict(lambda: 0)
@@ -86,70 +68,99 @@ def extract_training_data(inp):
     return td
 
 ######################################################################################
-### Workings of the model itself
+### Class for log-linear models
 
-def loglikelihood(m, td, weights):
-    probtable = probs_from_model(m, weights)
-    def logprob(x,y):
-        prob = probtable[x][y]
-        if prob == 0.0:
-            raise ProbabilityZeroError
-        else:
-            return np.log(prob)
-    try:
-        ll = sum([ freq * logprob(lhs,rhs) for ((lhs,rhs),freq) in td.items() ])
-    except ProbabilityZeroError:
-        ll = np.finfo('float').min  # the smallest number possible
-    return ll
+class LogLinModel():
 
-# This function corresponds to equation (6) in Mike Collins' notes here:
-#       http://www.cs.columbia.edu/~mcollins/loglinear.pdf
-# First note that the right-hand side of equation(6):
-#       sum_i [ f_k(x_i, y_i) ]  -  sum_i sum_y [ p(y, x_i, v) f_k(x_i, y) ]
-# can be re-expressed as
-#       sum_i [ f_k(x_i, y_i)  -  sum_y [ p(y, x_i, v) f_k(x_i, y) ] ]
-# This determines a number for each value of k; or, a vector with one entry 
-# for each value of k. That vector is what this function computes. 
-# The subtraction can be thought of as subtracting the expected contribution 
-# of the kth feature from the observed contribution.
-def loglhdgrad(m, td, weights):
+    # Sets up these instance variables:
+    #   self._rules: a mapping from LHSs to mappings from RHSs to feature vectors
+    #   self._dim: the number of parameters
+    def __init__(self, inp):
+        self._rules = defaultdict(lambda: {})
+        self._dim = None
+        for (lhs, rhs, feats) in inp:
+            if self._dim is None:
+                self._dim = len(feats)
+            else:
+                assert self._dim == len(feats), "Mismatching dimensions"
+            old_feats = self._rules[lhs].get(rhs, None)
+            if old_feats is not None:
+                assert np.array_equal(old_feats, feats), ("Mismatching features for lhs %s and rhs %s" % (lhs,rhs))
+            else:
+                self._rules[lhs][rhs] = np.array(feats)
 
-    dim = len(weights)
+    def dim(self):
+        return self._dim
 
-    # Precompute p(y,x,v) for each (x,y) pair
-    probtable = probs_from_model(m, weights)
+    def score(self, weights, x, y):
+        return np.dot(weights, self._rules[x][y])
 
-    # Precompute the vector 'sum_y [ p(y, x_i, v) f_k(x_i, y) ]' for each x
-    expectation = {}
-    for lhs in m:
-        foo = np.zeros(dim)
-        for (rhsp, featvec) in m[lhs].items():
-            foo += probtable[lhs][rhsp] * featvec
-        expectation[lhs] = foo
+    def probs_from_model(self, weights):
+        probtable = {}
+        for (x,ys) in self._rules.items():
+            probtable[x] = {}
+            scores = np.array([self.score(weights,x,y) for y in ys])
+            # Using the exp-normalize trick from here: https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
+            b = scores.max()
+            shifted_exp_scores = np.exp(scores - b)
+            normalized_probs = shifted_exp_scores / shifted_exp_scores.sum()
+            for (y, p) in zip(ys, normalized_probs):
+                probtable[x][y] = p
+        return probtable
 
-    # Now compute the overall result by cycling through the training data.
-    # Our 'freq' does not show up in Collins' equations: it's the number of 
-    # times this particular (lhs,rhs) pair shows up in Collins' i-indexed training set.
-    result = np.zeros(dim)
-    for ((lhs,rhs),freq) in td.items():
-        result += freq * (m[lhs][rhs] - expectation[lhs])
+    def loglikelihood(self, td, weights):
+        probtable = self.probs_from_model(weights)
+        def logprob(x,y):
+            prob = probtable[x][y]
+            if prob == 0.0:
+                raise ProbabilityZeroError
+            else:
+                return np.log(prob)
+        try:
+            ll = sum([ freq * logprob(lhs,rhs) for ((lhs,rhs),freq) in td.items() ])
+        except ProbabilityZeroError:
+            ll = np.finfo('float').min  # the smallest number possible
+        return ll
 
-    return result
+    # This function corresponds to equation (6) in Mike Collins' notes here:
+    #       http://www.cs.columbia.edu/~mcollins/loglinear.pdf
+    # First note that the right-hand side of equation(6):
+    #       sum_i [ f_k(x_i, y_i) ]  -  sum_i sum_y [ p(y, x_i, v) f_k(x_i, y) ]
+    # can be re-expressed as
+    #       sum_i [ f_k(x_i, y_i)  -  sum_y [ p(y, x_i, v) f_k(x_i, y) ] ]
+    # This determines a number for each value of k; or, a vector with one entry 
+    # for each value of k. That vector is what this function computes. 
+    # The subtraction can be thought of as subtracting the expected contribution 
+    # of the kth feature from the observed contribution.
+    def loglhdgrad(self, td, weights):
 
-def probs_from_model(m, weights):
-    probtable = {}
-    for (x,ys) in m.items():
-        probtable[x] = {}
-        scores = np.array([score(m,weights,x,y) for y in ys])
-        # Using the exp-normalize trick from here: https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
-        b = scores.max()
-        shifted_exp_scores = np.exp(scores - b)
-        normalized_probs = shifted_exp_scores / shifted_exp_scores.sum()
-        for (y, p) in zip(ys, normalized_probs):
-            probtable[x][y] = p
-    return probtable
+        # Precompute p(y,x,v) for each (x,y) pair
+        probtable = self.probs_from_model(weights)
 
-score = lambda m, weights, x, y: np.dot(weights, m[x][y])
+        # Precompute the vector 'sum_y [ p(y, x_i, v) f_k(x_i, y) ]' for each x
+        expectation = {}
+        for lhs in self._rules:
+            foo = np.zeros(self._dim)
+            for (rhsp, featvec) in self._rules[lhs].items():
+                foo += probtable[lhs][rhsp] * featvec
+            expectation[lhs] = foo
+
+        # Now compute the overall result by cycling through the training data.
+        # Our 'freq' does not show up in Collins' equations: it's the number of 
+        # times this particular (lhs,rhs) pair shows up in Collins' i-indexed training set.
+        result = np.zeros(self._dim)
+        for ((lhs,rhs),freq) in td.items():
+            result += freq * (self._rules[lhs][rhs] - expectation[lhs])
+
+        return result
+
+    def report_model(self, weights):
+        probtable = self.probs_from_model(weights)
+        print("######################################")
+        for lhs in self._rules:
+            for rhs in sorted(self._rules[lhs]):
+                print("%12.6f\t%.6f\t%s --> %s" % (self.score(weights,lhs,rhs), probtable[lhs][rhs], lhs, " ".join(rhs)))
+        print("######################################")
 
 ######################################################################################
 ### Regularization/priors; providing L2 regularization as the only option for now
@@ -162,29 +173,21 @@ def penaltygrad(lam, weights):
 
 ######################################################################################
 
-def report_model(m, weights):
-    probtable = probs_from_model(m, weights)
-    print("######################################")
-    for lhs in m:
-        for rhs in sorted(m[lhs]):
-            print("%12.6f\t%.6f\t%s --> %s" % (score(m,weights,lhs,rhs), probtable[lhs][rhs], lhs, " ".join(rhs)))
-    print("######################################")
-
 def run(filename, regularization_lambda):
 
     # This input data contains both rules' feature vectors and their training frequencies together
     input_data = list(read_input(filename))
-    (m,dim) = extract_model([(lhs,rhs,feats) for (freq,lhs,rhs,feats) in input_data])
+    m = LogLinModel([(lhs,rhs,feats) for (freq,lhs,rhs,feats) in input_data])
     td = extract_training_data([(freq,lhs,rhs) for (freq,lhs,rhs,feats) in input_data])
 
-    objective = lambda weights: penalty(regularization_lambda, weights) - loglikelihood(m, td, weights)
-    gradient = lambda weights: penaltygrad(regularization_lambda, weights) - loglhdgrad(m, td, weights)
-    res = minimize(objective, np.array([0.0 for x in range(dim)]), jac=gradient, method='BFGS', options={'disp':True})
+    objective = lambda weights: penalty(regularization_lambda, weights) - m.loglikelihood(td, weights)
+    gradient = lambda weights: penaltygrad(regularization_lambda, weights) - m.loglhdgrad(td, weights)
+    res = minimize(objective, np.array([0.0 for x in range(m.dim())]), jac=gradient, method='BFGS', options={'disp':True})
     print("Found optimal parameter values:", res.x)
     print("Objective function at this point:", objective(res.x))
-    print("                  Log likelihood:", loglikelihood(m, td, res.x))
+    print("                  Log likelihood:", m.loglikelihood(td, res.x))
     print("                         Penalty:", penalty(regularization_lambda, res.x))
-    report_model(m, res.x)
+    m.report_model(res.x)
 
 def main(argv):
     argparser = argparse.ArgumentParser()
